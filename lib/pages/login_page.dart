4f438a3017a114e6e5bf7701da4fe982e73dedb9
Himanshu <technophilic000@gmail.com>
2022-02-24 07:55:30 +0530

    Day 2

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Login_Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text("Login Page",style:TextStyle(
+          fontSize: 30,
+          color: Colors.blue,
+        ))
+                 ),
+    );
+  }
+}
